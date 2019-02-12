a9b2bc831cb5bb58acccc70a5fab43796ab72538
moisesmiss <moisesgamiss@gmail.com>
2019-02-11 20:45:10 -0600

    add font-awesome

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>Started Template</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!-- font awesome -->
+	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
 	
